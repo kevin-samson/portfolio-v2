2b0c7e3387a6b75fd50027942e5b8bd6cb156684
Kevin Samson <kevinsam2003@gmail.com>
2024-01-18 19:04:41 +0400

    basic outline

---
+++ context/smoothScroll.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState, useEffect, createContext } from "react";
+
+export const SmoothScrollContext = createContext<{
+  scroll: LocomotiveScroll | null;
+}>({
+  scroll: null,
+});
+
+export function SmoothScrollProvider({
+  children,
+  options,
+}: {
+  children: React.ReactNode;
+  options: any;
+}) {
+  const [scroll, setScroll] = useState<LocomotiveScroll | null>(null);
+
+  useEffect(() => {
+    if (!scroll) {
+      (async () => {
+        try {
+          const LocomotiveScroll = (await import("locomotive-scroll")).default;
+
+          setScroll(
+            new LocomotiveScroll({
+              el:
+                document.querySelector("[data-scroll-container]") ?? undefined,
+              mobile: {
+                smooth: true,
+              },
+              ...options,
+            })
+          );
+        } catch (error) {
+          throw Error(`[SmoothScrollProvider]: ${error}`);
+        }
+      })();
+    }
+    return () => {
+      scroll?.destroy();
+    };
+  }, [scroll]);
+
+  return (
+    // END: abpxx6d04wxr
+    <SmoothScrollContext.Provider value={{ scroll }}>
+      {children}
+    </SmoothScrollContext.Provider>
+  );
+}
+
+SmoothScrollContext.displayName = "SmoothScrollContext";
+SmoothScrollProvider.displayName = "SmoothScrollProvider";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      text: "var(--text)",
+      background: "var(--background)",
+      primary: "var(--primary)",
+      secondary: "var(--secondary)",
+      accent: "var(--accent)",
+      white: "var(--white)",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
+
   plugins: [],
-}
-export default config
+};
+export default config;
